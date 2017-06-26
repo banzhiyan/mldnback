30692f29e528f681e9278fc3cfe64e1e70757a1f
duzhimin <815295584@qq.com>
2017-06-26 18:15:55 +0800

    aaa

--- emp.java
+++ emp.java
@@ -1,12 +1,9 @@
 public class emp{
    public static void main(){
-<<<<<<< Updated upstream
       System.out.println("aaaaaaa");
 	  System.out.println("bbbb");
 	  System.out.println("bbbbb");
 	   System.out.println("cccc");
-=======
       System.out.println("bbbbbbbb");
->>>>>>> Stashed changes
    }
 }
